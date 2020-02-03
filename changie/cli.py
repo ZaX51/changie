b3a2a662d2a560862914dbd4816e07c935dddcba
Dawid Sołtysiak <d.soltysiak51@gmail.com>
2020-02-03 21:30:08 +0100

    generate changelog

--- changie/cli.py
+++ changie/cli.py
@@ -1,5 +1,6 @@
 import click
 from datetime import datetime
+import os
 
 @click.group()
 def main():
@@ -7,10 +8,37 @@ def main():
 
 @main.command()
 @click.option('-m', '--message', default='', help='message')
-@click.option('-t', '--type', default='Added', help='message type', type=click.Choice(['Added', 'Changed'], case_sensitive=False))
-def add(message, type):
-    create_changelog_item(message, type)
+def add(message):
+    create_changelog_item(message)
 
-def create_changelog_item(message, type):
-    with open('chg_{}_{}.md'.format(datetime.now().timestamp(), type), 'w+') as f:
-        f.write(message)
+def create_changelog_item(message):
+    write_file('chg_{}.md'.format(datetime.now().timestamp()), message)
+
+@main.command()
+@click.option('-v', '--version', default='New version', help='message')
+def generate(version):
+    new_changelog = '## {}\n\n'.format(version)
+    should_update = False
+
+    for filename in os.listdir(os.getcwd()):
+        name, file_extension = os.path.splitext(filename)
+        if file_extension == '.md' and filename.startswith('chg'):
+            should_update = True
+
+            new_changelog += '* {}\n'.format(read_file(filename))
+            os.remove(filename)
+    
+    if should_update == True:
+        current_changelog = read_file('CHANGELOG.md')
+        new_changelog += '\n'+ current_changelog
+
+        write_file('CHANGELOG.md', new_changelog)
+        
+
+def read_file(file):
+    with open(file, 'r') as f:
+        return f.read()
+
+def write_file(file, s):
+    with open(file, 'w+') as f:
+        return f.write(s)
