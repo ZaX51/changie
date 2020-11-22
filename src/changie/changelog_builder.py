dfefcc891173ea523d5b21a693fd1a0c0ac6247e
Dawid Sołtysiak <d.soltysiak51@gmail.com>
2020-11-22 14:34:46 +0100

    Print date in header

--- src/changie/changelog_builder.py
+++ src/changie/changelog_builder.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
+
 class ChangelogBuilder:
     def __init__(self):
         self.changelog = ""
 
     def add_header(self, version):
-        self.changelog += f"## {version}\n\n"
+        date = datetime.now().strftime("%m-%d-%Y")
+        self.changelog += f"## {version} - {date}\n\n"
 
     def add_changes_list(self, items):
         self.changelog += "\n".join(map(lambda item: f"* {item}", items)) + "\n"
